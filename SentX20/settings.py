c3f44734ba4ff664e6b0a953a57302fda1a26044
CelalettinOZGEN <celalettinozgen@yahoo.com>
2020-08-13 14:34:00 +0300

    version1

--- SentX20/settings.py
+++ SentX20/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*qy))dith=eejspeyt-6ad$1ps69+s6p&y3&5dsb4hk9q_qsj-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
